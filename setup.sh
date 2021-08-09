3c55f253c75a276579d5932ffe5a986c6082fd3a
ec965 <enoch965@gmail.com>
2021-08-09 14:14:50 -0700

    create .config if it's not there

--- setup.sh
+++ setup.sh
@@ -8,6 +8,8 @@ fi
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # power level 10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# .zshrc
+cp $PWD/.zshrc $HOME/.zshrc
 
 # mac apps
 if [[ "$OS" == "Darwin" ]]; then
@@ -22,7 +24,6 @@ if [[ "$OS" == "Linux" ]]; then
     sudo apt install neovim
 fi
 
-# .zshrc
-cp $PWD/.zshrc $HOME/.zshrc
 # symlink .config files
+[ ! -d "$HOME/.config" ] && mkdir $HOME/.config
 ln -s $PWD/nvim $HOME/.config/nvim
