d236caaa35b42dd125a8ead6f9c73eab5ce3671c
Enoch Chau <enoch@gatsby.events>
2021-12-14 00:25:48 -0600

    add context comment string for more langs

--- nvim/lua/enoch/kommentary.lua
+++ nvim/lua/enoch/kommentary.lua
@@ -1,5 +1,13 @@
-require('kommentary.config').configure_language('typescriptreact', {
-  single_line_comment_string = 'auto',
-  multi_line_comment_strings = 'auto',
-  hook_function = function() require('ts_context_commentstring.internal').update_commentstring() end
-})
+for _, lang in ipairs({
+  'typescriptreact',
+  'lua',
+  'vim',
+  'css',
+  'scss'
+}) do
+  require('kommentary.config').configure_language(lang, {
+    single_line_comment_string = 'auto',
+    multi_line_comment_strings = 'auto',
+    hook_function = function() require('ts_context_commentstring.internal').update_commentstring() end
+  })
+end
