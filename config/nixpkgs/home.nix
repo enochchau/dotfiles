4c4fa4a042befd3655da251ddc25901f312c93c4
ec965 <enoch965@gmail.com>
2022-01-16 21:01:42 -0800

    move tmux config under nix

--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }:
 
 {
+  targets.genericLinux.enable = true;
   home.username = "enoch";
   home.homeDirectory = "/home/enoch";
 
@@ -18,4 +19,23 @@
   home.stateVersion = "22.05";
 
   programs.home-manager.enable = true;
+  programs.tmux = {
+    enable = true;
+    shortcut = "a";
+    keyMode = "vi";
+    historyLimit = 10000;
+    extraConfig = ''
+      set -g mouse on
+
+      set -g default-terminal "screen-256color"
+      # tell Tmux that outside terminal supports true color
+      set -ga terminal-overrides ",xterm-256color*:Tc"
+
+      set -g status-style fg='black',bg='blue'
+      set -g status-right "%a %I:%M%p %d %b %Y"
+
+      set -g pane-border-style "fg=default bg=default"
+      set -g pane-active-border-style "fg=colour4 bg=default"
+    '';
+  };
 }
