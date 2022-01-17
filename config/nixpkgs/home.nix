05793d5d7a043bec9163134dcaa83b551f3cb9f9
ec965 <enoch965@gmail.com>
2022-01-16 21:34:46 -0800

    seperate apps into nix modules

--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -1,41 +1,10 @@
-{ config, pkgs, ... }:
+{ ... }:
 
 {
-  targets.genericLinux.enable = true;
-  home.username = "enoch";
-  home.homeDirectory = "/home/enoch";
-
-  home.packages = with pkgs; [
-    fzf
-    fd
-    ripgrep
-    bat
-    tealdeer
-    hyperfine
-    delta
-    tmux
+  imports = [
+    ./shared.nix
   ];
 
-  home.stateVersion = "22.05";
-
-  programs.home-manager.enable = true;
-  programs.tmux = {
-    enable = true;
-    shortcut = "a";
-    keyMode = "vi";
-    historyLimit = 10000;
-    extraConfig = ''
-      set -g mouse on
-
-      set -g default-terminal "screen-256color"
-      # tell Tmux that outside terminal supports true color
-      set -ga terminal-overrides ",xterm-256color*:Tc"
-
-      set -g status-style fg='black',bg='blue'
-      set -g status-right "%a %I:%M%p %d %b %Y"
-
-      set -g pane-border-style "fg=default bg=default"
-      set -g pane-active-border-style "fg=colour4 bg=default"
-    '';
-  };
+  home.username = "enoch";
+  home.homeDirectory = "/home/enoch";
 }
