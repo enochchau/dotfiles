cc26946cf1fe72903c46bd53565fe44c6efb21bb
ec965 <enoch965@gmail.com>
2022-02-07 00:55:59 -0800

    remove gitsigns nullls

--- home/config/nixpkgs/nvim/null-ls.lua
+++ home/config/nixpkgs/nvim/null-ls.lua
@@ -2,7 +2,6 @@ local null_ls = require("null-ls")
 
 -- register any number of sources simultaneously
 local sources = {
-  null_ls.builtins.code_actions.gitsigns,
   null_ls.builtins.formatting.prettierd.with({
     env = {
       PRETTIERD_DEFAULT_CONFIG = vim.fn.expand("~/.config/nixpkgs/.prettierrc"),
