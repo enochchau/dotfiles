d7aa8af48644911a419ddac43c8c244711f67020
ec965 <enoch965@gmail.com>
2022-02-09 00:27:05 -0800

    fix toggle term background for gruvbox light

--- home/config/nixpkgs/nvim/toggleterm.lua
+++ home/config/nixpkgs/nvim/toggleterm.lua
@@ -1,3 +1,7 @@
-require("toggleterm").setup({
+local opts = {
   open_mapping = [[<c-\>]],
-})
+}
+if vim.g.colors_name == "gruvbox" then
+  opts.shading_factor = 5
+end
+require("toggleterm").setup(opts)
