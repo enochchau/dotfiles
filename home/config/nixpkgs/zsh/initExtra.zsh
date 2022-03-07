ed76a27c36bb5fc6ab9645a35e1c3d356900f94f
ec965 <enoch965@gmail.com>
2022-03-06 22:01:07 -0800

    extract out cdg

--- home/config/nixpkgs/zsh/initExtra.zsh
+++ home/config/nixpkgs/zsh/initExtra.zsh
@@ -3,14 +3,6 @@ bindkey '^[[Z' autosuggest-accept  # shift + tab  | autosuggest
 export PATH="$(yarn global bin):$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 
-# go to git root directory
-cdg() { cd "$(git rev-parse --show-toplevel)/$1" }
-_cdg_completion()
-{
-  COMPREPLY="$(ls $(git rev-parse --show-toplevel || echo "$HOME")/)" 
-}
-complete -F _cdg_completion cdg
-
 if test -f $ZDOTDIR/machine.zshrc; then 
   source $ZDOTDIR/machine.zshrc
 fi
