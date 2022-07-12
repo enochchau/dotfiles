d616d74ee84dc0c3f18442c493b628890953f428
ec965 <22567131+ec965@users.noreply.github.com>
2022-07-12 00:20:03 -0700

    Use bg to change tokyonight color

--- roles/nvim/files/lua/enoch/theme.lua
+++ roles/nvim/files/lua/enoch/theme.lua
@@ -2,7 +2,7 @@ local colo = require("enoch.helpers").colo
 
 vim.opt.bg = "light"
 vim.g.neon_style = "light"
-vim.g.tokyonight_style = "day"
+vim.g.tokyonight_style = "storm"
 
 colo("tokyonight")
 -- colo("neon")
