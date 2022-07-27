3ecd634984d34f5bf528c44ae180a3e575b91a8e
ec965 <22567131+ec965@users.noreply.github.com>
2022-07-26 23:06:10 -0700

    Config theme for nvim 0.7

--- roles/nvim/files/lua/enoch/theme.lua
+++ roles/nvim/files/lua/enoch/theme.lua
@@ -3,8 +3,12 @@ local colo = require("enoch.helpers").colo
 vim.opt.bg = "light"
 vim.g.tokyonight_style = "storm"
 
--- colo("tokyonight")
--- colo("onedark")
--- colo("nightfox")
--- colo("doom-one")
-colo "juliana"
+-- colo "onedark"
+-- colo "nightfox"
+-- colo "doom-one"
+
+if vim.version().minor < 8 then
+    colo "juliana"
+else
+    colo "tokyonight"
+end
