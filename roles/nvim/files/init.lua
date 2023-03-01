c74bfd3f215492a6405dbd00b5ac772f40b262be
ec965 <enoch965@gmail.com>
2023-03-01 00:52:45 -0500

    Use floating window for vim.ui.input

--- roles/nvim/files/init.lua
+++ roles/nvim/files/init.lua
@@ -108,3 +108,57 @@ require "enoch.commands"
 -- theme
 opt.bg = "dark"
 vim.cmd.colorscheme "onedark"
+
+-- use floating input for renaming stuff
+vim.ui.input = function(opts, on_confirm)
+    local prompt = opts.prompt or "Input: "
+    local default = opts.default or ""
+
+    -- Calculate a minimal width with a bit buffer
+    local default_width = vim.str_utfindex(default) + 10
+    local prompt_width = vim.str_utfindex(prompt) + 10
+    local win_width = default_width > prompt_width and default_width
+        or prompt_width
+
+    local win_config = {
+        relative = "cursor",
+        row = 1,
+        col = 0,
+        width = win_width,
+        height = 1,
+        focusable = true,
+        style = "minimal",
+        border = "single",
+    }
+    if vim.fn.has("nvim-0.9") == 1 then
+        win_config.title = prompt
+    end
+
+
+    local buffer = vim.api.nvim_create_buf(false, true)
+    local window = vim.api.nvim_open_win(buffer, true, win_config)
+    vim.api.nvim_buf_set_text(buffer, 0, 0, 0, 0, { opts.default })
+
+    -- Put cursor at the end of the default value
+    vim.cmd "startinsert"
+    vim.api.nvim_win_set_cursor(window, { 1, vim.str_utfindex(default) + 1 })
+
+    -- Enter to confirm
+    vim.keymap.set({ "n", "i", "v" }, "<cr>", function()
+        local lines = vim.api.nvim_buf_get_lines(buffer, 0, 1, false)
+        if on_confirm then
+            on_confirm(lines[1])
+        end
+        vim.api.nvim_win_close(window, true)
+        vim.cmd "stopinsert"
+    end, { buffer = buffer })
+
+    -- Esc to close
+    vim.api.nvim_create_autocmd({ "BufLeave", "InsertLeave" }, {
+        buffer = buffer,
+        callback = function()
+            vim.api.nvim_win_close(window, true)
+            vim.cmd "stopinsert"
+        end,
+    })
+end
