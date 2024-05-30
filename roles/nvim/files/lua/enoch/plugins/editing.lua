1656b4db2be2735c7bd6d5e27bd30cd510c2147c
ec965 <enoch965@gmail.com>
2024-05-30 03:22:04 -0700

    update nvim, use comment.nvim

--- roles/nvim/files/lua/enoch/plugins/editing.lua
+++ roles/nvim/files/lua/enoch/plugins/editing.lua
@@ -52,20 +52,22 @@ return {
         },
     },
     {
-        "JoosepAlviste/nvim-ts-context-commentstring",
-        -- opts = { enable_autocmd = false },
+        "numToStr/Comment.nvim",
+        dependencies = {
+            {
+                "JoosepAlviste/nvim-ts-context-commentstring",
+                opts = { enable_autocmd = false },
+            },
+        },
+        config = function()
+            require("Comment").setup {
+                pre_hook = require(
+                    "ts_context_commentstring.integrations.comment_nvim"
+                ).create_pre_hook(),
+            }
+        end,
+        lazy = false,
     },
-    -- {
-    --     "numToStr/Comment.nvim",
-    --     config = function()
-    --         require("Comment").setup {
-    --             pre_hook = require(
-    --                 "ts_context_commentstring.integrations.comment_nvim"
-    --             ).create_pre_hook(),
-    --         }
-    --     end,
-    --     lazy = false,
-    -- },
     {
         "danymat/neogen",
         dependencies = "nvim-treesitter/nvim-treesitter",
