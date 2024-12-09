a1e448c1a296f032da625754c8b14fef969a21aa
ec965 <enoch965@gmail.com>
2024-12-09 11:54:17 -0800

    update conform config

--- roles/nvim/files/lua/enoch/plugins/editing.lua
+++ roles/nvim/files/lua/enoch/plugins/editing.lua
@@ -45,7 +45,7 @@ return {
                 function()
                     require("conform").format({
                         async = true,
-                        lsp_fallback = true,
+                        lsp_format = 'fallback',
                     })
                 end,
                 mode = "",
